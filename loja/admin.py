2f34460017ee2bb37f742ee3a5e925ee69d557d5
RAFAEL RICCO DANTAS CAVALCANTI <20221011110007@ifrn.local>
2024-04-01 08:18:11 -0300

    modelos

--- loja/admin.py
+++ loja/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
 # Register your models here.
 from .models import *
 admin.site.register(Fabricante)
+admin.site.register(Categoria)
+admin.site.register(Produto)
