6c1072641ce85b8794e98156f7208517d79ca10f
RAFAEL RICCO DANTAS CAVALCANTI <20221011110007@ifrn.local>
2024-07-08 08:27:02 -0300

    rafael tod

--- loja/admin.py
+++ loja/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 # Register your models here.
 from .models import *
-admin.site.register(Fabricante)
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+admin.site.register(Fabricante, FabricanteAdmin)
 admin.site.register(Categoria)
-admin.site.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+admin.site.register(Produto, ProdutoAdmin)
