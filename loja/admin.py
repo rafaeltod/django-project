7255a991f08d2be8c5247b56cba835306845d39c
RAFAEL RICCO DANTAS CAVALCANTI <20221011110007@ifrn.local>
2024-07-12 10:08:09 -0300

    vai toma toma

--- loja/admin.py
+++ loja/admin.py
@@ -4,10 +4,16 @@ from .models import *
 class FabricanteAdmin(admin.ModelAdmin):
     # Cria um filtro de hierarquia com datas
     date_hierarchy = 'criado_em'
+    search_fields = ('Fabricante',)
 admin.site.register(Fabricante, FabricanteAdmin)
-admin.site.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+    search_fields = ('Categoria',)
+admin.site.register(Categoria, CategoriaAdmin)
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
     list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
     empty_value_display = 'Vazio'
+    search_fields = ('Produto',)
 admin.site.register(Produto, ProdutoAdmin)
--- loja/models/Produto.py
+++ loja/models/Produto.py
@@ -11,5 +11,6 @@ related_name='categoria', on_delete=models.SET_NULL)
 related_name='fabricante', on_delete=models.SET_NULL)
     criado_em = models.DateTimeField(auto_now_add=True)
     alterado_em = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return '{}'.format(self.Produto)
