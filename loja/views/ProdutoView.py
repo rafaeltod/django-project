14ec5ac5d0c87da59ccba749a0434fcad0e928bc
RAFAEL RICCO DANTAS CAVALCANTI <20221011110007@ifrn.local>
2024-07-22 10:29:35 -0300

    e tome

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
+from loja.models import Produto
 def list_produto_view(request, id=None):
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    print(produtos)
     if id is None:
         return HttpResponse('<h1>Nenhum id foi informado</h1>')
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
