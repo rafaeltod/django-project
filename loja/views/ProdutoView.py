4f565e1e3eba9c669a8f2718057f5eb0c585bc37
RAFAEL RICCO DANTAS CAVALCANTI <20221011110007@ifrn.local>
2024-08-12 08:19:52 -0300

    toma

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
 def list_produto_view(request, id=None):
     produto = request.GET.get("produto")
     destaque = request.GET.get("destaque")
@@ -10,10 +18,8 @@ def list_produto_view(request, id=None):
     fabricante = request.GET.get("fabricante")
     dias = request.GET.get("dias")
     produtos = Produto.objects.all()
-    if dias is not None:
-        now = timezone.now()
-        now = now - timedelta(days = int(dias))
-        produtos = produtos.filter(criado_em__gte=now)
+    if id is not None:
+        produtos = produtos.filter(Produto__contains=produto)
     if produto is not None:
         produtos = produtos.filter(Produto=produto)
     if promocao is not None:
@@ -24,9 +30,12 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
         produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
     if id is not None:
-        produtos = produtos.filter(Produto__contains=produto)
+        produtos = produtos.filter(id=id)
     print(produtos)
-    if id is None:
-        return HttpResponse('<h1>Nenhum id foi informado</h1>')
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    context = {'produtos': produtos}
+    return render(request, template_name='produto/produto.html', context=context, status=200)
